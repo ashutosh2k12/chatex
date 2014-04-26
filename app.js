358ca560468c611589e482445caabce62f1b355a
Ashutosh <216.ashutosh@gmail.com>
2014-04-27 03:48:33 +0530

    init

--- app.js
+++ app.js
@@ -11,9 +11,7 @@ var express = require('express')
  */
 var app = express();
 
-/**
- * App configuration.
- */
+
 app.configure(function () {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
